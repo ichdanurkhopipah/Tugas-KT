2c3454d196676e7c16d5bcec7273a39ebdf672df
ichdanurkhopipah <ichda.nur.khopipah13tkj4@gmail.com>
2016-12-09 10:13:16 +0700

    Add files via upload

---
+++ Pengukur_suhu.ino
@@ -0,0 +1,22 @@
+//include&lt;LiquidCrystal.h&gt;;
+
+int Vin;
+float Temperature;
+float TF;
+
+//LiquidCrystal.lcd(2, 3, 4, 5, 6, 7);
+
+void setup(){
+//lcd.begin(16, 2);
+//lcd.print("Temperature:")
+}
+
+void loop(){
+Vin = analogRead(A0);
+Temperature=(500*Vin)/1023;
+//lcd.setCursor(0, 1);
+//lcd.print(Temperature);
+//lcd.print("C");
+delay(1000);
+}
+
